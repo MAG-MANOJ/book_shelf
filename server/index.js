1bfd8a90c61a8b68efbbced6a8125d42be544b2a
Manoj <gsmano14@gmail.com>
2023-11-13 22:20:26 +0530

    created Post method to add a new Book

--- server/index.js
+++ server/index.js
@@ -1,12 +1,36 @@
-import express from "express";
+import express, { response } from "express";
 import mongoose from "mongoose";
+import { Book } from "./model/bookModel.js";
 
 const app = express();
 const PORT = 3000;
 
-app.get('/',(req,res)=>{
-    res.status(200).send('The application works good');
+//Middleware
+app.use(express.json());
+
+app.get('/',(request,response)=>{
+    console.log(request.url);
+    response.status(200).send('The application works good');
 })
+
+app.post('/books',async (request, response) =>{
+    console.log(request.url);
+    try{
+        if(!request.body.title || !request.body.author || !request.body.publishYear){
+            response.status(400).send({message:'cannot access the required fields: title, auther, publishYear'});
+        };
+        const newBook = {
+            title: request.body.title,
+            author: request.body.author,
+            publishYear: request.body.publishYear
+        };
+        const addBook = await Book.create(newBook);
+        return response.status(200).send(addBook)
+    }catch(error){
+        console.log(error.message)
+    }
+});
+
 
 main().then(console.log('App connected with Database')).catch(err=>console.log(err.message));
 
