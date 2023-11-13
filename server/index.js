fc75fa4868b34c659a9890cb6e09444b7f9403ba
Manoj <gsmano14@gmail.com>
2023-11-13 20:43:53 +0530

    Added MongoDB and mongoose to node js

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import mongoose from "mongoose";
 
 const app = express();
 const PORT = 3000;
@@ -6,4 +7,10 @@ const PORT = 3000;
 app.get('/',(req,res)=>{
     res.status(200).send('The application works good');
 })
-app.listen(PORT,()=>console.log(`Application listen on Port ${PORT}...`));
+
+main().then(console.log('App connected with Database')).catch(err=>console.log(err.message));
+
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/book');
+    app.listen(PORT,()=>console.log(`Application listen on Port ${PORT}...`));
+}
