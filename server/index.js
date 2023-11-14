35f5510a0086ff214f9c184be3d7bd66b849364f
Manoj <gsmano14@gmail.com>
2023-11-14 11:01:54 +0530

    Added cors

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 import express, { request, response } from "express";
 import mongoose from "mongoose";
+import cors  from "cors";
 import booksRoute from "./routes/booksRoute.js";
 
 const app = express();
@@ -8,6 +9,11 @@ const PORT = 3000;
 //Middleware
 app.use(express.json());
 app.use('/books',booksRoute)
+app.use(cors({
+    origin:'http://localhost:3000',
+    methods:['GET','POST','PUT','DELETE'],
+    allowedHeaders: ['Content-Type']
+}))
 
 app.get('/',(request,response)=>{
     response.status(200).send('The application works good');
